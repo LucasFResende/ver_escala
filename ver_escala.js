2c030ad7be95b3a3b071975bd0aeba1b7efc8cc2
LucasFResende <lucasfresende2003@gmail.com>
2026-03-18 13:50:48 -0300

    Adição da funcionalidade de ler um arquivo para alteração de nomes, Alteração para a seleção dar cor de acordo com o arquivo de resposta

--- ver_escala.js
+++ ver_escala.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const escalaFile = document.getElementById("escala")?.files[0];
         const respostasFile = document.getElementById("resposta")?.files[0];
+        const conversaoFile = document.getElementById("conversao")?.files[0];
 
         if (!escalaFile || !respostasFile) {
             alert("Selecione os dois arquivos antes de enviar.");
@@ -20,17 +21,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
         let escala;
         let respostas;
+        let conversao;
 
         // Ler os dois arquivos
         escala = await lerArquivo(escalaFile, "ESCALA");
         respostas = await lerArquivo(respostasFile, "RESPOSTAS");
+        conversao = await lerArquivo(conversaoFile, "CONVERSAO");
 
-        var nomePorDia;
-        var nomePorHorario;
-        nomePorDia = getNomePorDia(escala);
-        nomePorHorario = getNomePorHorario(escala);
 
-        exibirEscala(escala, nomePorDia, nomePorHorario);
+        respostas = ajeitarRespostas(respostas, conversao);
+
+
+        // avaliarEscala(escala, respostas);
+
+        const nomePorDia = getNomePorDia(escala);
+        const nomePorHorario = getNomePorHorario(escala);
+        exibirEscala(escala, respostas, conversao);
     });
 
 });
@@ -55,12 +61,21 @@ function lerArquivo(file, nome) {
                 if (nome === "ESCALA") {
                     var ref = "A3:I14";
                     json = XLSX.utils.sheet_to_json(sheet, { range: ref, raw: false });
-                } else {
+                } else if (nome === "RESPOSTAS") {
                     const range = XLSX.utils.decode_range(sheet["!ref"]);
                     range.s.c = 1;
                     range.e.c = 3;
                     sheet["!ref"] = XLSX.utils.encode_range(range);
                     json = XLSX.utils.sheet_to_json(sheet, { range: ref, raw: false });
+                    for (let x in json) {
+                        const { ["Nome do acólito ( nome e sobrenome ) "]: nomeAcolito,
+                            ["Dias"]: dias,
+                            ...resto } = json[x];
+                        json[x] = { nome: nomeAcolito, restricao: dias, horario: Object.values(resto)[0] };
+                    }
+                } else if (nome === "CONVERSAO") {
+                    json = XLSX.utils.sheet_to_json(sheet, { raw: false });
+                    console.log(json);
                 }
 
                 resolve(json);
@@ -77,6 +92,37 @@ function lerArquivo(file, nome) {
 
         reader.readAsArrayBuffer(file);
     });
+}
+
+function ajeitarRespostas(respostas, conversao) {
+    respostas = respostas.filter(r =>
+        r.horario !== "NÃO VOU SERVIR ESSE MÊS"
+    );
+
+    for (let i = 0; i < respostas.length; i++) {
+        if (respostas[i].restricao === undefined) {
+            respostas[i].restricao = [];
+        } else {
+            respostas[i].restricao = respostas[i].restricao.split(",");
+        }
+        respostas[i].horario = respostas[i].horario.split(",");
+        for (let j = 0; j < respostas[i].horario.length; j++) {
+            if (respostas[i].horario[j].trim() === "SABADOS AS 17H") respostas[i].horario[j] = "17H";
+            if (respostas[i].horario[j].trim() === "DOMINGOS AS 7H") respostas[i].horario[j] = "7H";
+            if (respostas[i].horario[j].trim() === "DOMINGOS AS 9H ( Max 4 acolitos )") respostas[i].horario[j] = "9H";
+            if (respostas[i].horario[j].trim() === "DOMINGOS AS 11H") respostas[i].horario[j] = "11H";
+            if (respostas[i].horario[j].trim() === "DOMINGOS AS 17h" || respostas[i].horario[j].trim() === "DOMINGOS AS 17H") respostas[i].horario[j] = "17H_1";
+            if (respostas[i].horario[j].trim() === "DOMINGOS AS 19H") respostas[i].horario[j] = "19H";
+            if (respostas[i].horario[j].trim() === "SEM PREFERÊNCIAS") respostas[i].horario = ["17H", "7H", "9H", "11H", "17H_1", "19H"];
+        }
+        for (let j = 0; j < conversao.length; j++) {
+            if (conversao[j].NomeResposta.trim() === respostas[i].nome.trim()) {
+                respostas[i].nome = conversao[j].NomeEscala.trim();
+            }
+        }
+    }
+    console.log(respostas);
+    return respostas;
 }
 
 function separarNomes(texto) {
@@ -105,7 +151,6 @@ function getNomePorDia(escala) {
         ];
     }
 
-    console.log(nomePorDia);
     return nomePorDia;
 }
 
@@ -120,20 +165,35 @@ function getNomePorHorario(escala) {
         nomePorHorario["17H_1"].push(...separarNomes(escala[i]["17H_1"]));
         nomePorHorario["19H"].push(...separarNomes(escala[i]["19H"]));
     }
-    console.log(nomePorHorario);
     return nomePorHorario;
 
 
 }
 
-function verificarRespostas(nomes) {
+function verificarRespostas(nomes, dia, hor, respostas) {
     var status = ["CERTO", "ERRADO", "ATENCAO"]
 
     var retorno = {};
 
     for (let i = 0; i < nomes.length; i++) {
-        retorno[nomes[i]] = status[Math.floor(Math.random() * status.length)];
+        var achou = false;
+        for (let j = 0; j < respostas.length; j++) {
+            if (respostas[j].nome.trim() === nomes[i]) {
+                achou = true;
+                if (respostas[j].restricao.includes(dia)) {
+                    retorno[nomes[i]] = status[1];
+                } else if (!respostas[j].horario.includes(hor)) {
+                    retorno[nomes[i]] = status[1];
+                } else {
+                    retorno[nomes[i]] = status[0];
+                }
+            }
+        }
+        if (!achou) {
+            retorno[nomes[i]] = status[2];
+        }
     }
+    // console.log(retorno);
     return retorno;
 }
 
@@ -157,22 +217,22 @@ function adicionarCor(coluna, ver) {
     });
 }
 
-function adicionarLinhaNaTabelaComCor(linha) {
+function adicionarLinhaNaTabelaComCor(linha, respostas) {
     var diaSab = linha["SÁBADO"];
     var diaDom = linha["DOMINGO"];
 
     var missaSab17 = separarNomes(linha["17H"]);
-    var missaSab17ver = verificarRespostas(missaSab17);
+    var missaSab17ver = verificarRespostas(missaSab17, diaSab, "17H", respostas);
     var missaDom7 = separarNomes(linha["7H"]);
-    var missaDom7ver = verificarRespostas(missaDom7);
+    var missaDom7ver = verificarRespostas(missaDom7, diaDom, "7H", respostas);
     var missaDom9 = separarNomes(linha["9H"]);
-    var missaDom9ver = verificarRespostas(missaDom9);
+    var missaDom9ver = verificarRespostas(missaDom9, diaDom, "9H", respostas);
     var missaDom11 = separarNomes(linha["11H"]);
-    var missaDom11ver = verificarRespostas(missaDom11);
+    var missaDom11ver = verificarRespostas(missaDom11, diaDom, "11H", respostas);
     var missaDom17 = separarNomes(linha["17H_1"]);
-    var missaDom17ver = verificarRespostas(missaDom17);
+    var missaDom17ver = verificarRespostas(missaDom17, diaDom, "17H_1", respostas);
     var missaDom19 = separarNomes(linha["19H"]);
-    var missaDom19ver = verificarRespostas(missaDom19);
+    var missaDom19ver = verificarRespostas(missaDom19, diaDom, "19H", respostas);
 
     var linhaTabela = document.createElement("tr");
     for (let i = 0; i < 8; i++) {
@@ -199,7 +259,7 @@ function adicionarLinhaNaTabelaComCor(linha) {
     return linhaTabela;
 }
 
-function exibirEscala(escala, nomePorDia) {
+function exibirEscala(escala, respostas) {
     const localExibirEscala = document.getElementById("exibirEscala");
     localExibirEscala.innerHTML = "";
     const escalaCabecalho = document.createElement("thead");
@@ -215,9 +275,22 @@ function exibirEscala(escala, nomePorDia) {
     localExibirEscala.appendChild(escalaCabecalho);
     cabecalho[6] = "17H_1";
     for (let i = 0; i < escala.length; i++) {
-        var linha = adicionarLinhaNaTabelaComCor(escala[i]);
+        var linha = adicionarLinhaNaTabelaComCor(escala[i], respostas);
         escalaCorpo.appendChild(linha);
 
     }
     localExibirEscala.appendChild(escalaCorpo);
+}
+
+function avaliarEscala(escala, respostas) {
+    var nomePorDia;
+    var nomePorHorario;
+    nomePorDia = getNomePorDia(escala);
+    nomePorHorario = getNomePorHorario(escala);
+    var avaliacao = {}
+    console.log(escala);
+    console.log(nomePorDia);
+    console.log(nomePorHorario);
+
+
 }
